8ca8aec3fd4d5aa37c297628b460240827c256d5
bhargav794 <abhargav345@gmail.com>
2023-10-25 05:05:45 +0530

    Day 28 complete

---
+++ 28 - whack a mole/app.js
@@ -0,0 +1,57 @@
+const holes = document.querySelectorAll('.hole');
+  const scoreBoard = document.querySelector('.score');
+  const moles = document.querySelectorAll('.mole');
+
+  let lastHole;
+  let timeUp = false;
+  let score = 0;
+
+
+  function randomTime(min, max) {
+    return Math.round(Math.random()* (max-min)+min);
+  }
+
+  function randomHoles(holes) {
+    const idx = Math.floor(Math.random()*holes.length);
+    const hole = holes[idx];
+
+    if(hole === lastHole){
+        console.log("Different needed");
+        return randomHoles(holes);
+    }
+
+    lastHole = hole;
+    return hole;
+
+  }
+
+  function peep() {
+    const time = randomTime(200,2000);
+    const hole = randomHoles(holes);
+
+    hole.classList.add("up");
+
+    setTimeout(() => {
+        hole.classList.remove("up");
+        if(!timeUp) peep();
+    }, time);
+  }
+
+  function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+
+    score=0;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
+  }
+
+  function bonk(e) {
+    if(!e.isTrusted) return; //istrusted is false when a click is simulated.
+
+    score++;
+    this.classList.remove("up");
+    scoreBoard.textContent = score;
+  }
+
+  moles.forEach(mole => mole.addEventListener("click",bonk));
